f5a2db16d9328bf64aa42ddb0b2b1603eeb5c3bd
YannisS29 <127502575+YannisS29@users.noreply.github.com>
2023-03-10 13:43:39 +0100

    created api

---
+++ API/api/api/Controllers/QrController.cs
@@ -0,0 +1,20 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrController : ControllerBase
+    {
+        [HttpGet(Name = "GetQrResult")]
+        public QrCodeResult Get()
+        {
+            QrCodeResult qrCodeResult = new QrCodeResult();
+            qrCodeResult.Result = "Hello";
+
+            return qrCodeResult;       
+        }
+    }
+}
